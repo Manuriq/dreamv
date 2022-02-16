de1bc20286de5f3673ba57b08e7c20c7ec5c497b
Manu <at0mikmychaine@gmail.com>
2022-02-16 10:46:40 +0100

    Désactivation du module whitelist

--- resources/views/assets/nav-panel.blade.php
+++ resources/views/assets/nav-panel.blade.php
@@ -2,7 +2,7 @@
     <ul class="list-group">
         <a href="{{ route('panel.home') }}"><li class="list-group-item {{ (request()->is('panel/home')) ? 'active' : '' }}"><i class="fas fa-home fas-custom"></i>Mon Compte</li></a>
         @if (Auth::user()->whitelisted == 0)
-        <a href="{{ route('whitelist.index') }}"><li class="list-group-item {{ (request()->is('panel/whitelist')) ? 'active' : '' }}"><i class="far fa-address-card fas-custom"></i>Whitelist</li></a>
+
         @endif
         @if (Auth::user()->admin >= 4)
             <hr>
